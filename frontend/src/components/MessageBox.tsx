afb68c2b10e542f8ac6d889fd9b98df363a4e0f4
adiaz-uf <adiazufano13@gmail.com>
2025-06-06 11:34:37 +0200

    MessageBox, forms validations

---
+++ frontend/src/components/MessageBox.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { cn } from "../lib/utils";
+
+interface MessageBoxProps {
+  type: "success" | "error";
+  message: string;
+  show: boolean;
+}
+
+export const MessageBox: React.FC<MessageBoxProps> = ({ type, message, show }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (show) {
+      setVisible(true);
+    } else {
+      const timeout = setTimeout(() => setVisible(false), 300);
+      return () => clearTimeout(timeout);
+    }
+  }, [show]);
+
+  if (!show && !visible) return null;
+
+  return (
+    <div
+      className={cn(
+        "px-4 py-3 rounded-xl text-sm border w-70 ml-auto shadow-md transition-all duration-300 transform break-words overflow-hidden",
+        type === "error" && "bg-red-100 border-red-500 text-red-700",
+        type === "success" && "bg-green-100 border-green-500 text-green-700"
+      )}
+    >
+      {message}
+    </div>
+  );
+};
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,13 +1,84 @@
 "use client";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import AuthLayout from "../layouts/AuthLayout";
 import { Card, CardContent } from "../components/Card";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import { Link } from "react-router-dom";
+import { MessageBox } from "../components/MessageBox";
 
 export default function RegisterPage() {
+
+    const navigate = useNavigate();
+
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [new_password, setNewPassword] = useState<string>("");
+  const [repeat_password, setRepeatPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<boolean>(false);
+
+  function validatePassword(pw: string): boolean {
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    return regex.test(pw);
+  }
+
+  function validateEmail(em: string): boolean {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(em);
+  }
+
+  function handleRegister() {
+    if (!email  || !new_password || !repeat_password || !name) {
+      setError("Please fill all fields.");
+      setSuccess(false);
+      setTimeout(() => setError(null), 4000);
+      return;
+    }
+
+    if (new_password !== repeat_password) {
+      setError("Passwords must be the same.");
+      setSuccess(false);
+      setTimeout(() => setError(null), 4000);
+      return;
+    }
+
+    /* Password validation - TODO: Commented while developing */
+    /* if (!validatePassword(password)) {
+      setError(
+        "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character."
+      );
+      setSuccess(false);
+      setTimeout(() => setError(null), 3000);
+      return;
+    } */
+
+    /* email validation - TODO: Commented while developing */
+    /* if (!validateEmail(email)) {
+      setError("Invalid email format. Please enter a valid email address.");
+      setSuccess(false);
+      setTimeout(() => setError(null), 3000);
+      return;
+    } */
+
+    // Simulate successful login
+    setError(null);
+    setSuccess(true);
+    setTimeout(() => {
+      setSuccess(false);
+      navigate("/login");
+    }, 4000);
+  }
+
   return (
+    <div className="relative min-h-screen">
+      <div className="absolute top-4 right-4 z-50 space-y-2">
+      {error && <MessageBox type="error" message={error} show={!!error} />}
+      {success && <MessageBox type="success" message="Created account! You can Sign In now." show={success} />}
+    </div>
     <AuthLayout>
       <Card className="shadow-lg rounded-2xl">
         <CardContent className="p-6 space-y-6">
@@ -15,18 +86,22 @@ export default function RegisterPage() {
           <div className="space-y-4">
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">Full Name</label>
-              <Input placeholder="John Doe" className="focus:ring-pink-500 focus:border-pink-500" />
+              <Input placeholder="John Doe" value={name} onChange={(e) => setName(e.target.value)}/>
             </div>
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
-              <Input type="email" placeholder="you@example.com" className="focus:ring-pink-500 focus:border-pink-500" />
+              <Input type="email" placeholder="you@example.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700 mb-1">Password</label>
-              <Input type="password" placeholder="••••••••" className="focus:ring-pink-500 focus:border-pink-500" />
+              <label className="block text-sm font-medium text-gray-700 mb-1">New Password</label>
+              <Input type="password" placeholder="••••••••" value={new_password} onChange={(e) => setNewPassword(e.target.value)}/>
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">Repeat Password</label>
+              <Input type="password" placeholder="••••••••" value={repeat_password} onChange={(e) => setRepeatPassword(e.target.value)}/>
             </div>
           </div>
-          <Button className="w-full bg-pink-600 hover:bg-pink-700 text-white rounded-xl">
+          <Button className="w-full" onClick={handleRegister}>
             Sign Up
           </Button>
           <p className="text-sm text-center text-gray-500">
@@ -38,5 +113,6 @@ export default function RegisterPage() {
         </CardContent>
       </Card>
     </AuthLayout>
+    </div>
   );
 }
