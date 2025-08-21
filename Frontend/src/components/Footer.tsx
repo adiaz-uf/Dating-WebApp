ca3e0633075a3ce96eea2c13291369e3189bdaa7
adiaz-uf <adiazufano13@gmail.com>
2025-08-21 09:28:24 +0200

    new: footer

---
+++ Frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+export const Footer = () => {
+
+    return (
+        <footer className="bg-pink-50 shadow-md fixed top-0 left-0 w-full z-50 py-1 overflow-hidden">
+            <div className="text-center text-gray-500 text-sm mt-6">
+                © 2025 Matcha. All rights reserved.
+            </div>
+        </footer>
+    );
+
+}
